c3ddb13db1badd82439c1eca0bfcb6db1f2a3cf1
ebrima <ebrima.jobe92@gmail.com>
2021-01-31 02:18:42 -0800

    add player controls to the dom

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import {
   PlayerStore,
   usePlayerStore,
+  playerStoreSelectors,
 } from '../../stores/PlayerStore';
 import { TrackModel, useTracksStore } from '../../stores/TracksStore';
 import shallow from 'zustand/shallow';
@@ -20,7 +21,7 @@ import {
   IconSpeaker,
 } from '../icons';
 import { useMedia } from '../../workers/useMedia';
-
+import { MP3Player } from './player';
 const Player = () => {
   const playerSelectors = (state: PlayerStore) => ({
     playing: state.playing,
@@ -37,6 +38,7 @@ const Player = () => {
     volumeUp: state.volumeUp,
     volumeDown: state.volumeDown,
     mute: state.mute,
+    muted: playerStoreSelectors.muted(state),
     unmute: state.unmute,
     toggleMute: state.toggleMute,
     setCuePosition: state.setCuePosition,
@@ -62,6 +64,7 @@ const Player = () => {
     volumeDown,
     mute,
     unmute,
+    muted,
     toggleMute,
     setCuePosition,
     forward,
@@ -100,14 +103,24 @@ const Player = () => {
                 '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
             }}
           >
-            <iframe
-              key={currentTrack.key}
-              title={currentTrack.title}
-              width="100%"
-              height="120"
-              allow="autoplay"
-              src={currentTrack.url}
-              frameBorder="0"
+            <PlayerControls
+              volume={volume}
+              onVolumeChange={setVolume}
+              onPause={pause}
+              onResume={resume}
+              track={currentTrack}
+              playing={playing}
+              muted={muted}
+              onMute={mute}
+              onUnmute={unmute}
+              progress={progress}
+              onProgressChange={setProgress}
+              cuePosition={cuePosition}
+              onCuePositionChange={setCuePosition}
+              onForward={forward}
+              onRewind={rewind}
+              trackDuration={trackDuration}
+              setTrackDuration={setTrackDuration}
             />
           </div>
         </React.Fragment>
@@ -172,10 +185,12 @@ function PlayerControls({
 
   // TODO: Remove when mobile player done
   const useEmbed = useMemo(() => {
+    console.log('debug', debug, 'isMed', isMed);
     return debug || !isMed;
   }, [isMed, debug]);
 
   function onAudioProgress(progress: number) {
+    console.log(' playerProgress', playerProgress);
     if (!seeking) {
       setPlayerProgress(progress);
       onProgressChange(progress);
@@ -189,145 +204,142 @@ function PlayerControls({
 
   return (
     <React.Fragment>
-      {playerReady && (
-        <div
-          className={cx('gap-5 grid grid-cols-3 xl:grid-cols-10', {
-            hidden: useEmbed,
-          })}
-        >
-          <div className="xl:col-span-2 flex items-center space-x-3 ">
-            <div className="flex-shrink-0 h-16 w-16 rounded-lg overflow-hidden relative">
-              {/* <img
-                className="w-full h-full bg-gray-200"
-                src={track.picture_large}
-                alt={track.title}
-              /> */}
+      <div
+        className={cx('gap-5 grid grid-cols-3 xl:grid-cols-10', {
+          // hidden: useEmbed,
+        })}
+      >
+        <div className="xl:col-span-2 flex items-center space-x-3 ">
+          <div className="flex-shrink-0 h-16 w-16 rounded-lg overflow-hidden relative">
+            <div
+              className="w-full h-full bg-gray-200"
+              // src={track.picture_large}
+              // alt={track.title}
+            />
+          </div>
+          <div className="flex flex-col justify-center">
+            <div className="text-md font-bold leading-tight">
+              {track.title}
             </div>
-            <div className="flex flex-col justify-center">
-              <div className="text-md font-bold leading-tight">
-                {track.title}
-              </div>
-              <div className="text-gray-700 text-md">
-                {formatDate(track.lastModified)}
-              </div>
+            <div className="text-gray-700 text-md">
+              {formatDate(track.lastModified.toString())}
             </div>
           </div>
-          {/* Player */}
-          <div className="flex flex-col items-center justify-center xl:col-span-6 space-y-1">
-            <div className="flex items-center justify-center space-x-4">
-              <button
-                title="Rewind 30 seconds"
-                onClick={() => onRewind(30)}
-                className={cx(
-                  'bg-transparent rounded-full text-gray-700 p-2',
-                  'transition-all duration-200 ease-in-out',
-                  'hover:text-gray-900',
-                  'focus:outline-none focus:bg-gray-200',
-                )}
-              >
-                <IconBackThirty className="fill-current h-8 w-8" />
-              </button>
-              <button
-                onClick={() => (playing ? onPause() : onResume())}
-                className={cx(
-                  'p-2 rounded-full bg-indigo-600 border shadow-md text-white leading-none',
-                  'transition-all duration-200 ease-in-out',
-                  'hover:bg-indigo-700 hover:shadow-lg',
-                  'focus:outline-none focus:bg-indigo-700',
-                )}
-              >
-                {playing ? (
-                  <IconPause className="fill-current w-8 h-8 inline-block" />
-                ) : (
-                  <IconPlay className="fill-current w-8 h-8 inline-block" />
-                )}
-              </button>
-              <button
-                title="Forward 30 seconds"
-                onClick={() => onForward(30)}
-                className={cx(
-                  'bg-transparent rounded-full text-gray-700 p-2',
-                  'transition-all duration-200 ease-in-out',
-                  'hover:text-gray-900',
-                  'focus:outline-none focus:bg-gray-200',
-                )}
-              >
-                <IconSkipThirty className="fill-current h-8 w-8" />
-              </button>
-            </div>
-            <div className="max-w-3xl w-full">
-              <>
-                <div className="flex justify-center items-center">
-                  <div className="text-xs w-10 text-right">
-                    {formatTime(Math.ceil(playerProgress))}
-                  </div>
-                  <div className="flex flex-1 flex-col justify-center max-w-xl mx-3 relative w-full">
-                    <Slider
-                      max={trackDuration}
-                      value={playerProgress}
-                      onMouseDown={() => setSeeking(true)}
-                      onChange={(newVal) => {
-                        setPlayerProgress(newVal);
-                        lastSeekPos.current = newVal;
-                      }}
-                      onMouseUp={() => {
-                        setSeeking(false);
-                        onCuePositionChange(lastSeekPos.current);
-                      }}
-                    />
-                  </div>
-                  <div className="text-xs w-10">
-                    {formatTime(Math.ceil(trackDuration))}
-                  </div>
+        </div>
+        {/* Player */}
+        <div className="flex flex-col items-center justify-center xl:col-span-6 space-y-1">
+          <div className="flex items-center justify-center space-x-4">
+            <button
+              title="Rewind 30 seconds"
+              onClick={() => onRewind(30)}
+              className={cx(
+                'bg-transparent rounded-full text-gray-700 p-2',
+                'transition-all duration-200 ease-in-out',
+                'hover:text-gray-900',
+                'focus:outline-none focus:bg-gray-200',
+              )}
+            >
+              <IconBackThirty className="fill-current h-8 w-8" />
+            </button>
+            <button
+              onClick={() => (playing ? onPause() : onResume())}
+              className={cx(
+                'p-2 rounded-full bg-indigo-600 border shadow-md text-white leading-none',
+                'transition-all duration-200 ease-in-out',
+                'hover:bg-indigo-700 hover:shadow-lg',
+                'focus:outline-none focus:bg-indigo-700',
+              )}
+            >
+              {playing ? (
+                <IconPause className="fill-current w-8 h-8 inline-block" />
+              ) : (
+                <IconPlay className="fill-current w-8 h-8 inline-block" />
+              )}
+            </button>
+            <button
+              title="Forward 30 seconds"
+              onClick={() => onForward(30)}
+              className={cx(
+                'bg-transparent rounded-full text-gray-700 p-2',
+                'transition-all duration-200 ease-in-out',
+                'hover:text-gray-900',
+                'focus:outline-none focus:bg-gray-200',
+              )}
+            >
+              <IconSkipThirty className="fill-current h-8 w-8" />
+            </button>
+          </div>
+          <div className="max-w-3xl w-full">
+            <>
+              <div className="flex justify-center items-center">
+                <div className="text-xs w-10 text-right">
+                  {formatTime(Math.ceil(playerProgress))}
                 </div>
-              </>
-            </div>
-          </div>
-          {/* Volume */}
-          <div className="xl:col-span-2 flex items-center space-x-2 justify-end">
-            <div className="flex space-x-1 items-center">
-              <a
-                className={cx(
-                  'inline-block p-2 rounded-full',
-                  'transition-all duration-200 ease-in-out',
-                  'hover:bg-gray-200 ',
-                )}
-                title="Open in SoundCloud"
-                target="_blank"
-                rel="noopener noreferrer"
-                href={track.url}
-              >
-                <IconSoundcloud className="fill-current w-5 h-5" />
-              </a>
-              <div className="flex space-x-1 items-center">
-                <button
-                  className={cx(
-                    'inline-block p-1 rounded-full',
-                    'transition-all duration-200 ease-in-out',
-                    'hover:bg-gray-200',
-                    'focus:outline-none',
-                  )}
-                  title={muted ? 'Unmute' : 'Mute'}
-                  onClick={() => {
-                    muted ? onUnmute() : onMute();
-                  }}
-                >
-                  <IconSpeaker className="fill-current w-5 h-5" />
-                </button>
-                <div className="w-40 pr-4">
+                <div className="flex flex-1 flex-col justify-center max-w-xl mx-3 relative w-full">
                   <Slider
-                    min={0}
-                    max={100}
-                    value={volume}
-                    onChange={(val) => onVolumeChange(val)}
+                    max={trackDuration}
+                    value={playerProgress}
+                    onMouseDown={() => setSeeking(true)}
+                    onChange={(newVal) => {
+                      setPlayerProgress(newVal);
+                      lastSeekPos.current = newVal;
+                    }}
+                    onMouseUp={() => {
+                      setSeeking(false);
+                      onCuePositionChange(lastSeekPos.current);
+                    }}
                   />
                 </div>
+                <div className="text-xs w-10">
+                  {formatTime(Math.ceil(trackDuration))}
+                </div>
+              </div>
+            </>
+          </div>
+        </div>
+        <div className="xl:col-span-2 flex items-center space-x-2 justify-end">
+          <div className="flex space-x-1 items-center">
+            <a
+              className={cx(
+                'inline-block p-2 rounded-full',
+                'transition-all duration-200 ease-in-out',
+                'hover:bg-gray-200 ',
+              )}
+              title="Open in SoundCloud"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={track.url}
+            >
+              <IconSoundcloud className="fill-current w-5 h-5" />
+            </a>
+            <div className="flex space-x-1 items-center">
+              <button
+                className={cx(
+                  'inline-block p-1 rounded-full',
+                  'transition-all duration-200 ease-in-out',
+                  'hover:bg-gray-200',
+                  'focus:outline-none',
+                )}
+                title={muted ? 'Unmute' : 'Mute'}
+                onClick={() => {
+                  muted ? onUnmute() : onMute();
+                }}
+              >
+                <IconSpeaker className="fill-current w-5 h-5" />
+              </button>
+              <div className="w-40 pr-4">
+                <Slider
+                  min={0}
+                  max={100}
+                  value={volume}
+                  onChange={(val) => onVolumeChange(val)}
+                />
               </div>
             </div>
           </div>
         </div>
-      )}
-      {/* <SoundCloudPlayer
+      </div>
+      <MP3Player
         key={track.id}
         onReady={onPlayerReady}
         showNative={useEmbed}
@@ -336,7 +348,7 @@ function PlayerControls({
         playing={playing}
         volume={volume}
         onPlayProgressChange={onAudioProgress}
-      /> */}
+      />
     </React.Fragment>
   );
 }
