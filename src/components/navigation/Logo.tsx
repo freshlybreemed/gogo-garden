61e8c30e58e5c5fbdf7b17ceb1c45fd4e98cf815
ebrima <ebrima.jobe92@gmail.com>
2021-01-31 02:18:17 -0800

    updated logo h1

---
+++ src/components/navigation/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Soulector } from '../icons';
+
+export default function Logo() {
+  return (
+    <React.Fragment>
+      {/* <Soulector className="w-8 h-8 mr-2" /> */}
+      <h1 className="text-2xl font-bold">Gogo Garden</h1>
+    </React.Fragment>
+  );
+}
