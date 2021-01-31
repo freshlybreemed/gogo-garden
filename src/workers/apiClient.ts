a6c168f2f821fc7975df8e62b49d3c6c4f605448
ebrima <ebrima.jobe92@gmail.com>
2021-01-31 02:16:39 -0800

    artist store + api

--- src/workers/apiClient.ts
+++ src/workers/apiClient.ts
@@ -1,38 +1,57 @@
 import Amplify, { Storage } from 'aws-amplify';
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
 
 export function createApiClient() {
   return new APIClient();
 }
 
-
-export class APIClient {
-  get client() {
-    return Storage
-  }
-
-  getTracks(): Promise<TrackDTO[]>{
-    return this.client.list('')
-  }
-}
-
+export type ArtistDTO = {
+  id: string;
+  name: string;
+  shortName: string;
+  description?: string;
+  image?: string;
+  instagram?: string;
+  twitter?: string;
+  youtube?: string;
+};
 export type TrackDTO = {
   //  _id: string;
   // duration: number;
   // artist: string;
   // title: string;
-  // album: string; 
+  // album: string;
   // date:  Date;
-  key: number | string;
+  key: string;
   Tag: string;
   lastModified: Date;
   size: number;
+};
+export class APIClient {
+  get client() {
+    return Storage;
+  }
+
+  getTracks(): Promise<TrackDTO[]> {
+    return this.client.list('');
+  }
+
+  getArtists(): Promise<ArtistDTO[]> {
+    // return this.graphql.find('artists')
+    return this.client.list('');
+  }
 }
 
 export type GetTracksDTO = {
   tracks: TrackDTO[];
-}
+};
+
+export type GetArtistsDTO = {
+  artist: ArtistDTO[];
+};
 
 type UpdateEpisodeDTO = {
   msg: string;
-  retrievedTracks: string[]
-}
+  retrievedTracks: string[];
+};
