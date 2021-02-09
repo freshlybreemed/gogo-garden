5d14c1a89220176e479279fa985ba158677e6dce
ebrima <ebrima.jobe92@gmail.com>
2021-02-09 02:38:12 -0500

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -6,54 +6,6 @@ import Navigation from './components/navigation';
 
 export default function App() {
   const [searchText, setSearchText] = useState<string>('');
-
-  // const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  // const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault();
-  //   const songs = e.target.files;
-  //   if(songs){
-  //     for(var i = 0;i<songs.length; i++){
-  //       let tempSong = songs[i]
-  //       jsmediatags.read(tempSong, {
-  //         onSuccess: (tag: any) => {
-  //           const { tags: { artist, album } } = tag;
-  //           const song: File = { ...tempSong, artist, album }
-  //           console.log(song);
-  //           setSelectedFiles([...selectedFiles, song]);
-  //         },
-  //         onError: (error) => {
-  //           console.log(error);
-  //         },
-  //       });
-  //     }
-  //   }
-  //   console.log(selectedFiles);
-  // };
-
-  // const uploadSong = async (
-  //   e: React.MouseEvent<HTMLInputElement, MouseEvent>,
-  // ) => {
-  //   e.preventDefault();
-  //   selectedFiles.forEach((song: any) => {
-  //     Storage.put(`${song.artist}/${song.album}/${song.name}`, song, {
-  //       progressCallback(progress: any) {
-  //         console.log(
-  //           `Uploaded: ${Math.floor(
-  //             (progress.loaded / progress.total) * 100,
-  //           )}%`,
-  //         );
-  //       },
-  //     })
-  //       .then((result) => {
-  //         console.log(result);
-  //       })
-  //       .catch((err) => {
-  //         console.error(err);
-  //       });
-  //   });
-  //   console.log('done');
-  // };
-
   return (
     <div className="flex flex-col h-full text-gray-900">
       <Navigation
@@ -61,21 +13,6 @@ export default function App() {
         onSearchChange={setSearchText}
         onSearchClose={() => setSearchText('')}
       />
-      {/* <form id="upload-form">
-          <label className="custom-file-upload">
-            <input
-              onChange={handleChange}
-              type="file"
-              name="filename"
-              id="file-upload"
-              accept="audio/*"
-              multiple
-            />
-            Select Files
-          </label>
-          <input type="submit" onClick={uploadSong} value="Upload" />
-        </form>
-      */}
       <Library filterText={searchText} />
       <Player />
     </div>
