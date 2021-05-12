c4fbbafbb490b1e386ae437644b4d08458363bc7
ebrima <ebrima.jobe92@gmail.com>
2021-05-12 04:12:40 -0700

    fix firebase env var

--- src/workers/firebase/config.js
+++ src/workers/firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "garden-fe26e.firebaseapp.com",
-  projectId: "garden-fe26e",
-  storageBucket: "garden-fe26e.appspot.com",
-  messagingSenderId: "835999403649",
-  appId: "1:835999403649:web:801da0f4b0f23ced0a8780",
-  measurementId: "G-FSGERJTR9V"
+  apiKey: process.env.REACT_APP_VERCEL_FIREBASE_API_KEY,
+  authDomain: 'garden-fe26e.firebaseapp.com',
+  projectId: 'garden-fe26e',
+  storageBucket: 'garden-fe26e.appspot.com',
+  messagingSenderId: '835999403649',
+  appId: '1:835999403649:web:801da0f4b0f23ced0a8780',
+  measurementId: 'G-FSGERJTR9V',
 };
 
 
