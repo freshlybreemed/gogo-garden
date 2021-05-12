b5611c0e14b49d94cac21f036be45f42d9a11ff3
ebrima <ebrima.jobe92@gmail.com>
2021-05-12 03:51:20 -0700

    cookie work

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,50 @@
 import format from 'date-fns/format';
+import cookie from 'js-cookie';
+
+interface Process {
+  browser: boolean
+}
+declare var process: Process
+
+export const setCookie = (key: string, value: string) => {
+  if (process.browser) {
+    cookie.set(key, value, {
+      expires: 1,
+      path: '/',
+    });
+  }
+};
+
+export const removeCookie = (key: string) => {
+  if (process.browser) {
+    cookie.remove(key, {
+      expires: 1,
+    });
+  }
+};
+
+export const getCookie = (key: string, req: any) => {
+  return process.browser
+    ? getCookieFromBrowser(key)
+    : getCookieFromServer(key, req);
+};
+
+export const getCookieFromBrowser = (key: string) => {
+  return cookie.get(key);
+};
+
+const getCookieFromServer = (key: string, req: any) => {
+  if (!req.headers.cookie) {
+    return undefined;
+  }
+  const rawCookie = req.headers.cookie
+    .split(';')
+    .find((c: any) => c.trim().startsWith(`${key}=`));
+  if (!rawCookie) {
+    return undefined;
+  }
+  return rawCookie.split('=')[1];
+};
 
 export function clamp(num: number, min: number, max: number) {
   return Math.min(Math.max(num, min), max);
