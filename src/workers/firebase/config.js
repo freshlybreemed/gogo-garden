50b21d10230b09d85bd851ac41f93a2035d09212
ebrima <ebrima.jobe92@gmail.com>
2021-05-12 03:49:32 -0700

    firebase

---
+++ src/workers/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "garden-fe26e.firebaseapp.com",
+  projectId: "garden-fe26e",
+  storageBucket: "garden-fe26e.appspot.com",
+  messagingSenderId: "835999403649",
+  appId: "1:835999403649:web:801da0f4b0f23ced0a8780",
+  measurementId: "G-FSGERJTR9V"
+};
+
+
+export { firebaseConfig };
